3006c58418496a732de76a930691b65fe91987d1
yannicklansink <yannick.lansink@live.nl>
2021-08-24 13:18:56 +0200

    Updated the website

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,31 @@
+// Filters date formating
+const dateFilter = require('./src/filters/date-filter.js');
+const w3DateFilter = require('./src/filters/w3-date-filter.js');
+
+const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js');
+
 module.exports = config => {
+  // Add filters
+  config.addFilter('dateFilter', dateFilter);
+  config.addFilter('w3DateFilter', w3DateFilter);
+
   // Set directories to pass through to the dist folder
   config.addPassthroughCopy('./src/images/');
+  // Returns work items, sorted by display order
+  config.addCollection('work', collection => {
+    return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md'));
+  });
+
+  // Returns work items, sorted by display order then filtered by featured
+  config.addCollection('featuredWork', collection => {
+    return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md')).filter(
+      x => x.data.featured
+    );
+  });
+  // Returns a collection of blog posts in reverse date order
+  config.addCollection('blog', collection => {
+    return [...collection.getFilteredByGlob('./src/posts/*.md')].reverse();
+  });
   return {
     markdownTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
--- src/index.md
+++ src/index.md
@@ -1,5 +1,5 @@
 ---
-title: 'Hello, world'
+title: 'Yannick Lansink'
 layout: 'layouts/home.html'
 intro:
   eyebrow: 'Digital Marketing is our'
@@ -9,4 +9,19 @@ intro:
   buttonUrl: '/work'
   image: '/images/bg/toast.jpg'
   imageAlt: 'Buttered toasted white bread'
+primaryCTA:
+  title: 'This is an website made by Yannick Lansink'
+  summary: 'This is the project site you build when you take the “Learn
+    Eleventy From Scratch” course so it is all made up as a pretend
+    context. You will learn a lot about Eleventy by building this site
+    though. Take the course today!'
+  buttonText: 'Buy a copy'
+  buttonUrl: 'https://piccalil.li/course/learn-eleventy-from-scratch/'
+featuredWork:
+  title: 'Selected work'
+  summary: 'Some stuff that should give you an idea of what we’re all about.'
+studioFeed:
+  title: 'From inside the studio'
+
+
 ---
